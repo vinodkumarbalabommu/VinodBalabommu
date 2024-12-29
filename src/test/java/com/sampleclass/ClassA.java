247d415b30d8bbb1a65d2bd12b1e676dcf3b3cc7
vinodkumarbalabommu <vinodkumar.balabommu@gmail.com>
2024-12-29 16:55:05 +0530

    First time pushing the project new

--- src/test/java/com/sampleclass/ClassA.java
+++ src/test/java/com/sampleclass/ClassA.java
@@ -1,5 +1,21 @@
 package com.sampleclass;
 
 public class ClassA {
+	
+	private void method1() {
+		System.out.println("1");
+
+	}
+	
+	private void method2() {
+		System.out.println("2");
+
+	}
+	
+	private void method3() {
+		System.out.println("3");
+
+	}
+	
 
 }

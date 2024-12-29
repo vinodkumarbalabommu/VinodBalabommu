db61bf21e6151e424a25f57fb676624f89a7f2f9
vinodkumarbalabommu <vinodkumar.balabommu@gmail.com>
2024-12-29 17:15:47 +0530

    Added codes from Vinod Bala

---
+++ src/test/java/com/sampleclass/ClassB.java
@@ -0,0 +1,20 @@
+package com.sampleclass;
+
+public class ClassB {
+
+	private void webTesting() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void mobileTesting() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void apiTesting() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

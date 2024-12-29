1727c3c37d64ecf4bc902f3a33e93de539f77a3d
vinodkumarbalabommu <vinodkumar.balabommu@gmail.com>
2024-12-29 17:34:42 +0530

    Added codes from Vinod Nellore

--- src/test/java/com/sampleclass/ClassA.java
+++ src/test/java/com/sampleclass/ClassA.java
@@ -1,21 +1,35 @@
 package com.sampleclass;
 
 public class ClassA {
-	
+
 	private void method1() {
 		System.out.println("1");
 
 	}
-	
+
 	private void method2() {
 		System.out.println("2");
 
 	}
-	
+
 	private void method3() {
 		System.out.println("3");
 
 	}
-	
+
+	private void tamilNadu() {
+		System.out.println("Tamil");
+
+	}
+
+	private void andhra() {
+		System.out.println("Telugu");
+
+	}
+
+	private void kerala() {
+		System.out.println("Malayalam");
+
+	}
 
 }
